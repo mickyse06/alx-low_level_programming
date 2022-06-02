23b8794ee56ffea9aff60300081785bc71ab3301
mickyse06 <mickyse06@gmail.com>
2022-06-02 20:35:42 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,15 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+* main - Entry point
+* size of various types on the computer
+* Return: Printing size
+*/
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n");
+	printf("Size of an int: 4 byte(s)\n");
+	printf("Size of a long int: 4 byte(s)\n");
+	printf("Size of a long long int: 8 byte(s)\n");
+	printf("Size of a float: 4 byte(s)\n");
+	return (0);
+}
